7db6cee6b55bcaca5dba80ddc63302058005eb7c
franckfongang <franckfongang1980@gmail.com>
2022-04-11 12:45:20 -0400

    name change

---
+++ dockerfile.sh
@@ -0,0 +1,15 @@
+docker build . -t <Alexander fongang>/node-web-app
+
+{
+  "name": "docker_web_app",
+  "version": "1.0.0",
+  "description": "Node.js on Docker",
+  "author": "Alexander Fongang",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.16.1"
+  }
+}
